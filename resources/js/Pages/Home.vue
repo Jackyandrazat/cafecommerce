68ef0f25f834ac9f0a61a3cda199363513e2f8be
Jackyandrazat <Jackyandrazat@gmail.com>
2024-12-31 16:05:04 +0700

    update minor

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,40 +1,40 @@
 <template>
     <div>
-      <h1>Welcome to CafeCommerce</h1>
+        <h1>Welcome to CafeCommerce</h1>
 
-      <!-- Tombol PrimeVue -->
-      <Button label="Tambah ke Keranjang" icon="pi pi-shopping-cart" @click="addToCart" />
+        <!-- Tombol PrimeVue -->
+        <Button label="Tambah ke Keranjang" icon="pi pi-shopping-cart" @click="addToCart" />
 
-      <!-- Tabel PrimeVue -->
-      <DataTable :value="products" tableStyle="min-width: 50rem">
-        <Column field="name" header="Nama Produk"></Column>
-        <Column field="price" header="Harga"></Column>
-        <Column header="Aksi">
-          <template #body="slotProps">
-            <Button icon="pi pi-plus" label="Tambah" @click="addToCart(slotProps.data)" />
-          </template>
-        </Column>
-      </DataTable>
+        <!-- Tabel PrimeVue -->
+        <DataTable :value="products" tableStyle="min-width: 50rem">
+            <Column field="name" header="Nama Produk"></Column>
+            <Column field="price" header="Harga"></Column>
+            <Column header="Aksi">
+                <template #body="slotProps">
+                    <Button icon="pi pi-plus" label="Tambah" @click="addToCart(slotProps.data)" />
+                </template>
+            </Column>
+        </DataTable>
     </div>
-  </template>
+</template>
 
-  <script setup>
-  import Button from 'primevue/button';
-  import DataTable from 'primevue/datatable';
-  import Column from 'primevue/column';
+<script setup>
+import Button from 'primevue/button';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
-  defineProps({
+defineProps({
     products: Array,
-  });
+});
 
-  function addToCart(product) {
+function addToCart(product) {
     console.log('Produk ditambahkan:', product);
-  }
-  </script>
+}
+</script>
 
-  <style>
-  h1 {
+<style>
+h1 {
     font-size: 24px;
     margin-bottom: 1rem;
-  }
-  </style>
+}
+</style>
