e5f16bb93e4cc9ae2fdc7c52a71d1334a0c01403
Jackyandrazat <Jackyandrazat@gmail.com>
2024-12-31 16:02:06 +0700

    set primevue and modfiy home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,27 +1,40 @@
 <template>
     <div>
       <h1>Welcome to CafeCommerce</h1>
-      <div v-for="product in products" :key="product.id">
-        <h2>{{ product.name }}</h2>
-        <p>{{ product.price }}</p>
-        <button @click="addToCart(product)">Add to Cart</button>
-      </div>
+
+      <!-- Tombol PrimeVue -->
+      <Button label="Tambah ke Keranjang" icon="pi pi-shopping-cart" @click="addToCart" />
+
+      <!-- Tabel PrimeVue -->
+      <DataTable :value="products" tableStyle="min-width: 50rem">
+        <Column field="name" header="Nama Produk"></Column>
+        <Column field="price" header="Harga"></Column>
+        <Column header="Aksi">
+          <template #body="slotProps">
+            <Button icon="pi pi-plus" label="Tambah" @click="addToCart(slotProps.data)" />
+          </template>
+        </Column>
+      </DataTable>
     </div>
   </template>
 
   <script setup>
-  import { router } from '@inertiajs/vue3';
+  import Button from 'primevue/button';
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
 
   defineProps({
     products: Array,
   });
 
   function addToCart(product) {
-    router.post('/cart/add', {
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      quantity: 1,
-    });
+    console.log('Produk ditambahkan:', product);
   }
   </script>
+
+  <style>
+  h1 {
+    font-size: 24px;
+    margin-bottom: 1rem;
+  }
+  </style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import '../css/app.css';
 
 createInertiaApp({
@@ -8,6 +10,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .mount(el);
     },
 });
